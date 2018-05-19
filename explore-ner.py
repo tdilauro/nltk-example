caec25044d4a49a912abc9b4a8d8fa5aa8be4f5f
Tim DiLauro <tim.di.lauro+gitkraken@gmail.com>
2018-05-18 21:03:35 -0400

    factor out main() function

--- explore-ner.py
+++ explore-ner.py
@@ -11,17 +11,68 @@ import re
 DEFAULT_FILENAME = 'articles.json'
 DEFAULT_RECORD = None
 
-parser = argparse.ArgumentParser()
-parser.add_argument('filename', default=DEFAULT_FILENAME, nargs='?', help='echo the string you use here')
-parser.add_argument('record', default=DEFAULT_RECORD, nargs='?', help='record number (all, if omitted)')
-args = parser.parse_args()
 
-filename = args.filename
-specific_record = args.record
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', default=DEFAULT_FILENAME, nargs='?', help='echo the string you use here')
+    parser.add_argument('record', default=DEFAULT_RECORD, nargs='?', help='record number (all, if omitted)')
+    args = parser.parse_args()
+
+    filename = args.filename
+    specific_record = args.record
+
+
+    docs, doc_ids = load_articles(filename)
+
+    for i,doc in enumerate(docs):
+        sentences = sent_tokenize(doc)
+        for j,s in enumerate(sentences):
+            toks = word_tokenize(s)
+            print(" >> " + s)
+            #for t in toks:
+            #  print("      - " + t)
+            sentences[j] = toks
+        tagged = nltk.pos_tag_sents(sentences)
+        #print(tagged)
+        named_entities = nltk.ne_chunk_sents(tagged)
+
+        print(" ** NLTK NER ** ")
+        ner = {}
+        for stree in named_entities:
+            for subtree in stree.subtrees(filter=lambda t: len(t.label()) > 0):
+                type = subtree.label()
+                if type != 'S':
+                    name = " ".join(c[0] for c in subtree.leaves())
+                    #print(str(type) + ": " + name)
+                    try:
+                        ner[ str(type) + ": " + name ] += 1
+                    except:
+                        ner[ str(type) + ": " + name ] = 1
+        mylocation = ''
+        topgspgpe = ''
+        for w in sorted(ner, key=ner.get, reverse=True):
+            if ner[w] < 2:
+                break
+            m = re.search('^(\w+):\s+(.+)', w)
+            if m is not None:
+                t = m.group(1)
+                v = m.group(2)
+            print("   ", ner[w], w, " TYPE: ", t, "  ", v)
+            if t == "LOCATION":
+                mylocation = v
+            if mylocation == '' and topgspgpe == '' and (t == 'GSP' or t == 'GPE'):
+                topgspgpe = v
+        if mylocation == '':
+            mylocation = topgspgpe
+            if mylocation == '':
+                mylocation = 'undetected'
+        print("LOCATION: ", mylocation)
+
 
 def removeNonAscii(s): return "".join(i for i in s if ord(i)<128)
 
-def load_articles():
+
+def load_articles(filename):
     document_contents = []
     doc_ids = []
 
@@ -32,48 +83,6 @@ def load_articles():
             document_contents.append(row['content'])
     return document_contents, doc_ids
 
-docs, doc_ids = load_articles()
 
-for i,doc in enumerate(docs):
-    sentences = sent_tokenize(doc)
-    for j,s in enumerate(sentences):
-        toks = word_tokenize(s)
-        print(" >> " + s)
-        #for t in toks:
-        #  print("      - " + t)
-        sentences[j] = toks
-    tagged = nltk.pos_tag_sents(sentences)
-    #print(tagged)
-    named_entities = nltk.ne_chunk_sents(tagged)
-
-    print(" ** NLTK NER ** ")
-    ner = {}
-    for stree in named_entities:
-        for subtree in stree.subtrees(filter=lambda t: len(t.label()) > 0):
-            type = subtree.label()
-            if type != 'S':
-                name = " ".join(c[0] for c in subtree.leaves())
-                #print(str(type) + ": " + name)
-                try:
-                    ner[ str(type) + ": " + name ] += 1
-                except:
-                    ner[ str(type) + ": " + name ] = 1
-    mylocation = ''
-    topgspgpe = ''
-    for w in sorted(ner, key=ner.get, reverse=True):
-        if ner[w] < 2:
-            break
-        m = re.search('^(\w+):\s+(.+)', w)
-        if m is not None:
-            t = m.group(1)
-            v = m.group(2)
-        print("   ", ner[w], w, " TYPE: ", t, "  ", v)
-        if t == "LOCATION":
-            mylocation = v
-        if mylocation == '' and topgspgpe == '' and (t == 'GSP' or t == 'GPE'):
-            topgspgpe = v
-    if mylocation == '':
-        mylocation = topgspgpe
-        if mylocation == '':
-            mylocation = 'undetected'
-    print("LOCATION: ", mylocation)
+if __name__ == '__main__':
+    main()
